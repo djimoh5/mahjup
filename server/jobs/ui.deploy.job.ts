afaa47a014650d89dbb2c68dbcb60a352d7f6f67
Deji <deji.jimoh@gmail.com>
2026-06-19 16:26:36 -0500

    new logo and old assets deleted

--- server/jobs/ui.deploy.job.ts
+++ server/jobs/ui.deploy.job.ts
@@ -61,7 +61,9 @@ export class UIDeployJob extends Job {
     }
 
     private async oneTimeDeploy(_bucket: string) {
-        //await this.uploadFile('../dist/favicon.ico', 'favicon.ico', _bucket, 'no-cache');
+        await this.uploadFile('../dist/favicon.ico', 'favicon.ico', _bucket, 'no-cache');
+        await this.uploadFile('../Assets/mahjup-logo-dark.png', 'mahjup-logo-dark.png', _bucket);
+        await this.uploadFile('../Assets/mahjup-logo-light.png', 'mahjup-logo-light.png', _bucket);
     }
 
     private async deleteFiles(bucket: string, continuationToken?: string) {
